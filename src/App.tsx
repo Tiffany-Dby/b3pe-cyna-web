e39122d9c1af4ae8a6733a93ec5464dc0c638ba7
Tiffany-Dby <tiffany.dby@gmail.com>
2025-05-25 22:03:56 +0200

    add(chatbot): crisp sdk chatbot

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import BaseLayout from "@/shared/ui/components/BaseLayout";
 import { Toaster } from "sonner";
 import { usePurchaseStore } from "@/purchase/store/purchaseStore";
 import { useEffect } from "react";
+import { Crisp } from "crisp-sdk-web";
 
 const App = () => {
   const { getCart } = usePurchaseStore();
 
   useEffect(() => {
+    // Crisp.configure(import.meta.env.VITE_CRISP_TOKEN);
     getCart();
   }, []);
 
